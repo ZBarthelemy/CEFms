ff53eb3e5058259d0d95ae1d89186981a1bd04db
ZBarthelemy <edward.a.barthelemy@gmail.com>
2019-03-02 23:33:44 +0900

    add static method for 2 sigma from mean

--- pycef/client.py
+++ pycef/client.py
@@ -2,6 +2,7 @@ import requests
 import bs4
 from datetime import datetime
 from pycef.fund import Fund
+import pandas
 
 
 class Client(object):
@@ -22,17 +23,18 @@ class Client(object):
         else:
             soup = bs4.BeautifulSoup(r.text, 'lxml')
             as_of_text = (soup.find("span", id="ContentPlaceHolder1_cph_main_cph_main_AsOfLabel")
-                          .text
-                          .split(' ')[2])
+                .text
+                .split(' ')[2])
             as_of = datetime.strptime(as_of_text, '%m/%d/%Y').date()
             today_table_soup = soup.find(name='table', id='ContentPlaceHolder1_cph_main_cph_main_SummaryGrid')
             current_row = today_table_soup.findAll('tr')[1]
             row_columns = current_row.find_all('td')
             share_price = float(row_columns[1].text[1:])
             net_price = float(row_columns[2].text[1:])
-            premium_p = float(row_columns[3].text[:-1])/100
+            premium_p = float(row_columns[3].text[:-1]) / 100
             return Fund(name=ticker,
                         share_price=share_price,
                         net_asset_value=net_price,
                         current_premium_to_nav=premium_p,
-                        as_of=as_of)
+                        as_of=as_of,
+                        client=self._client)
--- pycef/fund.py
+++ pycef/fund.py
@@ -1,5 +1,7 @@
+import json
 from datetime import date
-
+import requests
+from pandas.io.json import json_normalize
 
 class Fund(object):
     def __init__(self,
@@ -7,12 +9,14 @@ class Fund(object):
                  share_price: float,
                  net_asset_value: float,
                  current_premium_to_nav: float,
-                 as_of: date):
+                 as_of: date,
+                 client: requests.Session):
         self.name = name
         self.share_price = share_price
         self.net_asset_value = net_asset_value
         self.current_premium_to_nav = current_premium_to_nav
         self.as_of = as_of
+        self._client = client
 
     def __eq__(self, other):
         if isinstance(other, Fund):
@@ -22,3 +26,18 @@ class Fund(object):
                     and self.current_premium_to_nav == other.current_premium_to_nav
                     and self.as_of == other.as_of)
         return False
+
+    def is_present_discount_2sigma_plus(self) -> bool:
+        fund_history = "https://www.cefconnect.com/api/v3/pricinghistory/" + self.name.upper() + "/1Y"
+        r = self._client.get(fund_history)
+        payload = json.loads(r.content)
+        df = json_normalize(payload['Data']['PriceHistory'])
+        present_discount = df.DiscountData.iat[-1]
+        st_dev = df.DiscountData.std()
+        avg = df.DiscountData.mean()
+
+        df.to_csv(r'/Users/whitestallion/Desktop/JPS.csv')
+        if (avg - 2 * st_dev) > present_discount:
+            return True
+        else:
+            return False
